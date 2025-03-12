f78dd527a2da1737617e8513ca34ac1fdaba1456
Manas Kishore <manaskishoredev@gmail.com>
2025-03-12 19:07:35 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,8 +1,8 @@
 window.env = {
-    apiKey: "AIzaSyBCQpRAaUoUG4UyE4D9nVMHKh4fxz9uzoo",
-    projectId: "chat-app-77771",
-    storageBucket: "chat-app-77771.firebasestorage.app",
-    messagingSenderId: "943621496980",
-    authDomain: "chat-app-77771.firebaseapp.com",
-    appId: "1:943621496980:web:a8d50488822243c7e11481"
+    apiKey: "YOUR API KEY",
+    projectId: "YOUR PROJECT ID",
+    storageBucket: "FIREBASE STORAGE BUCKET",
+    messagingSenderId: "ID",
+    authDomain: "FIREBASE DOMAIN ID",
+    appId: "YOUR APP IP"
 };
